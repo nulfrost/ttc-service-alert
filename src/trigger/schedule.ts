519862f8a708a6434ed451aaf437c3479b5e2c4e
nulfrost <khadane.miller@gmail.com>
2024-11-23 17:36:22 -0500

    fixed bug where we don't paginate when at 1000 records causing spam posts

--- src/trigger/schedule.ts
+++ src/trigger/schedule.ts
@@ -1,4 +1,4 @@
-import { wait, schedules, queue } from '@trigger.dev/sdk/v3';
+import { wait, schedules } from '@trigger.dev/sdk/v3';
 
 const env = {
 	CLOUDFLARE_API_TOKEN: process.env.CLOUDFLARE_API_TOKEN as string,
@@ -29,44 +29,79 @@ async function getMostRecentCachedAlert({ alerts }) {
 }
 
 async function writeDataToCloudflareKV({ timestamp, alerts }) {
-	const stringifiedAlertData = stringify(alerts);
+	try {
+		const stringifiedAlertData = stringify(alerts);
 
-	await fetch(
-		`https://api.cloudflare.com/client/v4/accounts/f9305b888ff3b829102d355476ae8793/storage/kv/namespaces/9dd30d2aaeb64605aa2758a7eb5f1452/values/${timestamp}`,
-		{
-			method: 'PUT',
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization: `Bearer ${env.CLOUDFLARE_API_TOKEN}`,
-			},
-			body: stringifiedAlertData,
-		},
-	);
+		await fetch(
+			`https://api.cloudflare.com/client/v4/accounts/f9305b888ff3b829102d355476ae8793/storage/kv/namespaces/f5267b57827b4e92bfdfca2f129c0606/values/${timestamp}`,
+			{
+				method: 'PUT',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${env.CLOUDFLARE_API_TOKEN}`,
+				},
+				body: stringifiedAlertData,
+			}
+		);
+	} catch (error) {
+		throw error;
+	}
+}
+
+interface CloudflareKVResponse {
+	result: Array<unknown>;
+	success: boolean;
+	errors: Array<unknown>;
+	messages: Array<unknown>;
+	result_info: { count: number; cursor: string };
 }
 
 async function listKVKeys() {
+	let cursor = await getCursor();
+
+	// we need to check if the result_info.count is equal to 1000, it means we've reached the limit for the current page
+	// a cursor is returned at this point which we need to use for the next page(s)
 	const response = await fetch(
-		'https://api.cloudflare.com/client/v4/accounts/f9305b888ff3b829102d355476ae8793/storage/kv/namespaces/9dd30d2aaeb64605aa2758a7eb5f1452/keys',
+		`https://api.cloudflare.com/client/v4/accounts/f9305b888ff3b829102d355476ae8793/storage/kv/namespaces/f5267b57827b4e92bfdfca2f129c0606/keys${
+			typeof cursor !== 'undefined' ? `?cursor=${cursor}` : ''
+		}`,
 		{
 			headers: {
 				'Content-Type': 'application/json',
 				Authorization: `Bearer ${env.CLOUDFLARE_API_TOKEN}`,
 			},
-		},
+		}
 	);
 
-	return response.json();
+	const data: CloudflareKVResponse = await response.json();
+
+	if (data.result_info.count === 1000 && data.result_info.cursor !== '') {
+		const response = await fetch(
+			`https://api.cloudflare.com/client/v4/accounts/f9305b888ff3b829102d355476ae8793/storage/kv/namespaces/f5267b57827b4e92bfdfca2f129c0606/keys?cursor=${data?.result_info?.cursor}`,
+			{
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${env.CLOUDFLARE_API_TOKEN}`,
+				},
+			}
+		);
+		await updateCursor(data?.result_info?.cursor);
+
+		return response.json();
+	}
+
+	return data;
 }
 
 async function getValueByKey(key: string) {
 	const response = await fetch(
-		`https://api.cloudflare.com/client/v4/accounts/f9305b888ff3b829102d355476ae8793/storage/kv/namespaces/9dd30d2aaeb64605aa2758a7eb5f1452/values/${key}`,
+		`https://api.cloudflare.com/client/v4/accounts/f9305b888ff3b829102d355476ae8793/storage/kv/namespaces/f5267b57827b4e92bfdfca2f129c0606/values/${key}`,
 		{
 			headers: {
 				'Content-Type': 'application/json',
 				Authorization: `Bearer ${env.CLOUDFLARE_API_TOKEN}`,
 			},
-		},
+		}
 	);
 
 	return response.json();
@@ -84,20 +119,16 @@ async function createThreadsMediaContainer({
 	userId,
 	accessToken,
 	postContent,
-	shouldQuotePost,
-	quotePostId,
 }: {
 	userId: string;
 	accessToken: string;
 	postContent: string;
-	shouldQuotePost: boolean;
-	quotePostId: string;
 }) {
 	const response = await fetch(
 		`https://graph.threads.net/v1.0/${userId}/threads?media_type=text&text=${postContent}&access_token=${accessToken}`,
 		{
 			method: 'POST',
-		},
+		}
 	);
 	const {
 		id,
@@ -126,7 +157,7 @@ async function publishThreadsMediaContainer({
 		`https://graph.threads.net/v1.0/${userId}/threads_publish?creation_id=${mediaContainerId}&access_token=${accessToken}`,
 		{
 			method: 'POST',
-		},
+		}
 	);
 
 	const { error } = await response.json();
@@ -145,7 +176,7 @@ async function insertIds({ alert_id, threads_post_id }) {
 				Authorization: `Bearer ${env.CLOUDFLARE_API_TOKEN}`,
 			},
 			body: `{"params":["${alert_id}","${threads_post_id}"],"sql":"INSERT into posts VALUES (?, ?);"}`,
-		},
+		}
 	);
 
 	return response.json();
@@ -161,10 +192,44 @@ async function findAlertById(alert_id) {
 				Authorization: `Bearer ${env.CLOUDFLARE_API_TOKEN}`,
 			},
 			body: `{"params":["${alert_id}"],"sql":"SELECT alert_id, threads_post_id FROM posts WHERE alert_id = ?;"}`,
-		},
+		}
 	);
 
 	return response.json();
+}
+
+async function updateCursor(cursor: string) {
+	const response = await fetch(
+		'https://api.cloudflare.com/client/v4/accounts/f9305b888ff3b829102d355476ae8793/d1/database/dedb959d-8767-464a-8030-b9a58a4b92c4/query',
+		{
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${env.CLOUDFLARE_API_TOKEN}`,
+			},
+			body: `{"params":["${cursor}"],"sql":"INSERT into cursors VALUES (?) ON CONFLICT DO UPDATE SET cursor=${cursor};"}`,
+		}
+	);
+
+	return response.json();
+}
+
+async function getCursor() {
+	const response = await fetch(
+		'https://api.cloudflare.com/client/v4/accounts/f9305b888ff3b829102d355476ae8793/d1/database/dedb959d-8767-464a-8030-b9a58a4b92c4/query',
+		{
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${env.CLOUDFLARE_API_TOKEN}`,
+			},
+			body: `{"sql":"SELECT * FROM cursors ORDER BY id DESC LIMIT 1;"}`,
+		}
+	);
+
+	const data = await response.json();
+
+	return data.result[0].results[0].cursor;
 }
 
 async function sendThreadsPost({ alertsToBePosted, alertsToBeCached, lastUpdatedTimestamp }) {
@@ -175,41 +240,32 @@ async function sendThreadsPost({ alertsToBePosted, alertsToBeCached, lastUpdated
 
 	try {
 		for (const alert of alertsToBePosted) {
-			const data = await findAlertById(alert.id);
-
-			const quotePostId = data?.result[0]?.results[0]?.threads_post_id;
-
+			// const data = await findAlertById(alert.id);
+			// const quotePostId = data?.result[0]?.results[0]?.threads_post_id;
 			const { id, error: mediaContainerError } = await createThreadsMediaContainer({
 				userId: env.THREADS_USER_ID,
 				accessToken: env.THREADS_ACCESS_TOKEN,
-				shouldQuotePost: data?.result[0]?.results?.length !== 0,
-				quotePostId,
 				postContent: encodeURIComponent(`
 			   	${alert.headerText}
 			   `),
 			});
-
-			if (data?.result[0]?.results?.length === 0) {
-				// this isn't an id that exists in the database already, we need to add it
-				await insertIds({ alert_id: +alert.id, threads_post_id: id });
-			}
-
+			// if (data?.result[0]?.results?.length === 0) {
+			// 	// this isn't an id that exists in the database already, we need to add it
+			// 	await insertIds({ alert_id: +alert.id, threads_post_id: id });
+			// }
 			if (mediaContainerError) {
 				console.error(`there was an error creating the media container: ${mediaContainerError.message}`);
 				return;
 			}
-
 			const { error: mediaPublishError } = await publishThreadsMediaContainer({
 				userId: env.THREADS_USER_ID,
 				accessToken: env.THREADS_ACCESS_TOKEN,
 				mediaContainerId: id,
 			});
-
 			if (mediaPublishError) {
 				console.error(`there was an error publishing the media container: ${mediaPublishError.message}`);
 				return;
 			}
-
 			await wait.for({ seconds: 60 });
 		}
 		console.log(`${alertsToBePosted.length} new threads post created on: ${new Date().toISOString()}`);
@@ -218,18 +274,16 @@ async function sendThreadsPost({ alertsToBePosted, alertsToBeCached, lastUpdated
 	}
 }
 
-const alertQueue = queue({
-	name: 'ttc-alert-queue',
-	concurrencyLimit: 1,
-});
-
 export const scheduledThreadsPost = schedules.task({
 	id: 'scheduled-threads-post',
 	cron: {
 		pattern: '* * * * *',
 		timezone: 'America/Toronto',
 	},
-	run: async (payload: any, { ctx }) => {
+	queue: {
+		concurrencyLimit: 1,
+	},
+	run: async () => {
 		try {
 			const alerts = await fetchTTCAlerts();
 
@@ -238,15 +292,15 @@ export const scheduledThreadsPost = schedules.task({
 
 			const listOfAlerts = await listKVKeys();
 
-			if (listOfAlerts.result.length === 0) {
-				console.info('no cached alerts, creating a new threads post');
-				await sendThreadsPost({
-					alertsToBePosted: alertsSortedByMostRecentTimestamp,
-					alertsToBeCached: filteredAlerts,
-					lastUpdatedTimestamp: alerts.lastUpdated,
-				});
-				return;
-			}
+			// if (listOfAlerts.result.length === 0) {
+			// 	console.info('no cached alerts, creating a new threads post');
+			// 	await sendThreadsPost({
+			// 		alertsToBePosted: alertsSortedByMostRecentTimestamp,
+			// 		alertsToBeCached: filteredAlerts,
+			// 		lastUpdatedTimestamp: alerts.lastUpdated,
+			// 	});
+			// 	return;
+			// }
 
 			const { lastCachedAlertData } = await getMostRecentCachedAlert({
 				alerts: listOfAlerts,
