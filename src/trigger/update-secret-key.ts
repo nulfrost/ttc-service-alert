c5c6e323fdfa264ee734baf73f8971cdf3afa104
nulfrost <khadane.miller@gmail.com>
2025-07-20 10:04:18 -0400

    fix update threads access token code

--- src/trigger/update-secret-key.ts
+++ src/trigger/update-secret-key.ts
@@ -1,4 +1,4 @@
-import { envvars, schedules } from "@trigger.dev/sdk/v3";
+import { envvars, logger, schedules } from "@trigger.dev/sdk/v3";
 import { reportErrorToDiscord } from "~/webhooks";
 
 export const updateSecretKey = schedules.task({
@@ -9,27 +9,27 @@ export const updateSecretKey = schedules.task({
 	},
 	run: async () => {
 		try {
-			const currentAccessToken = await envvars.retrieve(
-				"proj_imoofkrlqdujjamdlryj",
-				"prod",
+			const env = await envvars.retrieve(
 				"THREADS_ACCESS_TOKEN",
 			);
 
+
 			const response = await fetch(
-				`https://graph.threads.net/refresh_access_token?grant_type=th_refresh_token&access_token=${currentAccessToken}`,
+				`https://graph.threads.net/refresh_access_token?grant_type=th_refresh_token&access_token=${env.value}`,
 			);
 
-			const refreshedAccessToken = await response.json();
+			const data = await response.json();
 			await envvars.update("THREADS_ACCESS_TOKEN", {
-				value: refreshedAccessToken,
+				value: data.access_token,
 			});
+			logger.info(`access token updated at ${new Date().toISOString()}`)
 		} catch (error) {
 			console.log(
 				"there was an error updating the threads access token",
 				error,
 			);
 
-			await reportErrorToDiscord({ title: 'could not refresh access token', description: JSON.stringify(error) })
+			await reportErrorToDiscord({ title: 'could not refresh access token', context: JSON.stringify(error) })
 
 			throw error
 		}
